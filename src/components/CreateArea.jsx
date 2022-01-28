a0e229ff7b9e67d6b06bc971bc17123bbf8a203d
vajpayeemrudul <mrudulrcoem@gmail.com>
2022-01-28 12:48:34 +0530

    implement notes, styled-componenets

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,65 +1,79 @@
-import React, { useState } from "react";
+import React from 'react';
+import { useState } from 'react';
+import styled from 'styled-components';
 import AddIcon from '@material-ui/icons/Add';
 import Fab from '@material-ui/core/Fab';
 
 function CreateArea(props) {
 
-  const[bool ,setBool]=useState(false);
+  const [check,setCheck]=useState(false);
+  const [note,setNote]=useState({title:"",content:""});
 
-  const [note, setNote] = useState({
-    title: "",
-    content: ""
-  });
-
-  function handleChange(event) {
-    const { name, value } = event.target;
-
-    setNote(prevNote => {
-      return {
-        ...prevNote,
-        [name]: value
+  function handleChange(event)
+  {
+    const {name,value}=event.target;
+    setNote(previous=>{
+      return{
+        ...previous,[name]:value
       };
-    });
-  }
-
-  function submitNote(event) {
-    props.onAdd(note);
-    setNote({
-      title: "",
-      content: ""
-    });
-    event.preventDefault();
+    })
   }
 
   function expand()
   {
-    setBool(true);
+    setCheck(true);
+  }
+
+  function submitNote(event)
+  {
+     props.onAdd(note);
+     setNote({note:"",content:""});
+     event.preventDefault();
   }
 
   return (
-    <div>
-      <form className="create-note">
-        {bool && <input
-          name="title"
-          onChange={handleChange}
-          value={note.title}
-          placeholder="Title"
-        />}
-        <textarea
-          name="content"
-          onChange={handleChange}
-          onClick={expand}
-          value={note.content}
-          placeholder="Take a note..."
-          rows={bool ? 3:1}
-        />
-      
-        <Fab onClick={submitNote}>
-             <AddIcon />
-        </Fab>
-      </form>
-    </div>
+   <CreateNote>
+     {check && <input name="title" onChange={handleChange} value={note.title} placeholder='Title'/>}
+     <textarea name="content" onChange={handleChange} value={note.content} placeholder='Content' onClick={expand} rows={check ? 3:1}/>
+     <Fab onClick={submitNote}><AddIcon /></Fab>
+   </CreateNote>
+
   );
 }
 
 export default CreateArea;
+
+const CreateNote=styled.div`
+position: relative;
+width: 480px;
+margin: 30px auto 20px auto;
+background: #fff;
+padding: 15px;
+border-radius: 7px;
+box-shadow: 0 1px 5px rgb(138, 137, 137);
+
+input,textarea {
+    width: 100%;
+    border: none;
+    padding: 4px;
+    outline: none;
+    font-size: 1.2em;
+    font-family: inherit;
+    resize: none;
+  };
+  button {
+    position: absolute;
+    right: 18px;
+    bottom: -18px;
+    background: #f5ba13;
+    color: #fff;
+    border: none;
+    border-radius: 50%;
+    width: 36px;
+    height: 36px;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.3);
+    cursor: pointer;
+    outline: none;
+  };
+
+`
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,18 +1,50 @@
 import React from "react";
+import styled from "styled-components";
 import DeleteIcon from '@material-ui/icons/Delete';
 
 function Note(props) {
+
   function handleClick() {
     props.onDelete(props.id);
   }
-
   return (
-    <div className="note">
-      <h1>{props.title}</h1>
+     <Notetext>
+       <h1>{props.title}</h1>
       <p>{props.content}</p>
       <button onClick={handleClick}><DeleteIcon /></button>
-    </div>
+     </Notetext>
   );
 }
 
 export default Note;
+
+const Notetext=styled.div`
+    background: #fff;
+    border-radius: 7px;
+    box-shadow: 0 2px 5px #ccc;
+    padding: 10px;
+    width: 240px;
+    margin: 16px;
+    float: left;
+    h1 {
+      font-size: 1.1em;
+      margin-bottom: 6px;
+    };
+    p {
+      font-size: 1.1em;
+      margin-bottom: 10px;
+      white-space: pre-wrap;
+      word-wrap: break-word;
+    };
+    button {
+      position: relative;
+      float: right;
+      margin-right: 10px;
+      color: #f5ba13;
+      border: none;
+      width: 36px;
+      height: 36px;
+      cursor: pointer;
+      outline: none;
+    };
+`
